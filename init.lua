c5014aa2571bc81386f521453a85b0c1a7251db6
Ernesto Matos <ernesto.matos@loggi.com>
2024-10-21 09:59:42 -0300

    nvim: show hovering documentation

--- init.lua
+++ init.lua
@@ -206,6 +206,9 @@ require("lazy").setup({
 					--  For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
+					-- Shows documentation in a hovering panel.
+					map("K", vim.lsp.buf.hover, "Hover documentation")
+
 					-- The following two autocommands are used to highlight references of the
 					-- word under your cursor when your cursor rests there for a little while.
 					--    See `:help CursorHold` for information about when this is executed
