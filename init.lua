7e6fcd5032393895fa9ebe61c532842ad50366d3
Ernesto Matos <ernesto.matos@loggi.com>
2024-11-05 15:08:24 -0300

    nvim: C-u and C-d movements should be centered vertically

--- init.lua
+++ init.lua
@@ -540,3 +540,7 @@ vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 -- move selected text
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- U and D movements to center
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
