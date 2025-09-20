b4c6a82794226e297a90c5b6d8249c8970242328
Ernesto Cid <cid.ernesto@gmail.com>
2025-09-20 13:44:29 -0300

    cleaning up init.vim, adding new shortcuts and illuminate plugin

--- init.lua
+++ init.lua
@@ -18,6 +18,7 @@ require("lazy").setup({
 	{ "jeffkreeftmeijer/vim-numbertoggle" },
 	{ "nvim-telescope/telescope.nvim", tag = "0.1.8" },
 	{ "github/copilot.vim" },
+	{ "RRethy/vim-illuminate" },
 	{ "preservim/nerdcommenter" },
 	{
 		"nvim-treesitter/nvim-treesitter",
@@ -150,9 +151,9 @@ require("lazy").setup({
 			local protocol = require("vim.lsp.protocol")
 
 			--  This function gets run when an LSP attaches to a particular buffer.
-			--    That is to say, every time a new file is opened that is associated with
-			--    an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
-			--    function will be executed to configure the current buffer
+			--  That is to say, every time a new file is opened that is associated with
+			--  an lsp (for example, opening `main.rs` is associated with `rust_analyzer`) this
+			--  function will be executed to configure the current buffer
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
 				callback = function(event)
@@ -167,8 +168,8 @@ require("lazy").setup({
 					end
 
 					-- Jump to the definition of the word under your cursor.
-					--  This is where a variable was first declared, or where a function is defined, etc.
-					--  To jump back, press <C-t>.
+					-- This is where a variable was first declared, or where a function is defined, etc.
+					-- To jump back, press <C-t>.
 					map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
 					-- Find references for the word under your cursor.
@@ -179,16 +180,16 @@ require("lazy").setup({
 					map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
 					-- Jump to the type of the word under your cursor.
-					--  Useful when you're not sure what type a variable is and you want to see
-					--  the definition of its *type*, not where it was *defined*.
+					-- Useful when you're not sure what type a variable is and you want to see
+					-- the definition of its *type*, not where it was *defined*.
 					map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
 					-- Fuzzy find all the symbols in your current document.
-					--  Symbols are things like variables, functions, types, etc.
+					-- Symbols are things like variables, functions, types, etc.
 					map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
 					-- Fuzzy find all the symbols in your current workspace.
-					--  Similar to document symbols, except searches over your entire project.
+					-- Similar to document symbols, except searches over your entire project.
 					map(
 						"<leader>ws",
 						require("telescope.builtin").lsp_dynamic_workspace_symbols,
@@ -196,7 +197,7 @@ require("lazy").setup({
 					)
 
 					-- Rename the variable under your cursor.
-					--  Most Language Servers support renaming across files, etc.
+					-- Most Language Servers support renaming across files, etc.
 					map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
 					-- Execute a code action, usually your cursor needs to be on top of an error
@@ -204,41 +205,11 @@ require("lazy").setup({
 					map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
 
 					-- WARN: This is not Goto Definition, this is Goto Declaration.
-					--  For example, in C this would take you to the header.
+					-- For example, in C this would take you to the header.
 					map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 					-- Shows documentation in a hovering panel.
 					map("K", vim.lsp.buf.hover, "Hover documentation")
-
-					-- The following two autocommands are used to highlight references of the
-					-- word under your cursor when your cursor rests there for a little while.
-					--    See `:help CursorHold` for information about when this is executed
-					--
-					-- When you move your cursor, the highlights will be cleared (the second autocommand).
-					local client = vim.lsp.get_client_by_id(event.data.client_id)
-					if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
-						local highlight_augroup =
-							vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
-						vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.document_highlight,
-						})
-
-						vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
-							buffer = event.buf,
-							group = highlight_augroup,
-							callback = vim.lsp.buf.clear_references,
-						})
-
-						vim.api.nvim_create_autocmd("LspDetach", {
-							group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
-							callback = function(event2)
-								vim.lsp.buf.clear_references()
-								vim.api.nvim_clear_autocmds({ group = "kickstart-lsp-highlight", buffer = event2.buf })
-							end,
-						})
-					end
 
 					-- The following code creates a keymap to toggle inlay hints in your
 					-- code, if the language server you are using supports them
@@ -251,18 +222,6 @@ require("lazy").setup({
 					end
 				end,
 			})
-			local on_attach = function(client, bufnr)
-				-- format on save
-				if client.server_capabilities.documentFormattingProvider then
-					vim.api.nvim_create_autocmd("BufWritePre", {
-						group = vim.api.nvim_create_augroup("Format", { clear = true }),
-						buffer = bufnr,
-						callback = function()
-							vim.lsp.buf.format()
-						end,
-					})
-				end
-			end
 
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
@@ -529,10 +488,10 @@ vim.api.nvim_set_keymap("n", "<leader>cfa", "<cmd>CpAbsPath<cr>", { noremap = tr
 vim.api.nvim_set_keymap("n", "<leader>cfr", "<cmd>CpRelPath<cr>", { noremap = true })
 
 -- Find files using Telescope command-line sugar.
-vim.api.nvim_set_keymap("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fb", "<cmd>Telescope buffers<cr>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<leader>fh", "<cmd>Telescope help_tags<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>ff", "<cmd>Telescope find_files<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>fg", "<cmd>Telescope live_grep<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>fb", "<cmd>Telescope buffers<cr>", { noremap = true })
+vim.api.nvim_set_keymap("n", "<space>fh", "<cmd>Telescope help_tags<cr>", { noremap = true })
 
 -- git.nvim
 vim.keymap.set("n", "<leader>gb", '<CMD>lua require("git.blame").blame()<CR>')
@@ -553,3 +512,15 @@ vim.keymap.set("n", "N", "Nzzzv")
 
 -- nvim-tree
 vim.keymap.set("n", "<C-i>", "<cmd>NvimTreeFindFile<cr>")
+
+-- Remove search highlight
+vim.keymap.set("n", "<Leader><space>", ":nohlsearch<CR>")
+
+-- vim-illuminate Remaps
+local illuminate = require("illuminate")
+-- delete the defaults
+pcall(vim.keymap.del, "n", "<A-n>")
+pcall(vim.keymap.del, "n", "<A-p>")
+-- new keymaps
+vim.keymap.set("n", "<C-n>", illuminate.goto_next_reference, { desc = "Next Reference" })
+vim.keymap.set("n", "<C-p>", illuminate.goto_prev_reference, { desc = "Prev Reference" })
